b8eb3005a4818df54c7337f52cd7a6ebefc745a1
Yan Shang <sunnieshang@gmail.com>
2015-10-24 22:48:29 -0700

    Change 8

--- KN_BUpdate.m
+++ KN_BUpdate.m
@@ -27,7 +27,6 @@ function Exp_mat = KN_BUpdate(T_index, ID_mat, prior_mat, Exp_mat, vip_route)
         sub = zeros(1, size(categ_mat, 2)+1);
         levels_in_category(1) = vip_route(ID(i));
         n_obs = zeros(1, sum(levels_in_category));
-        sum_obs = zeros(1, sum(levels_in_category));
         for k = 1: n_category
             if (k==1) 
                 sub(k) = 0;
@@ -36,17 +35,17 @@ function Exp_mat = KN_BUpdate(T_index, ID_mat, prior_mat, Exp_mat, vip_route)
             end
             for j = 1: levels_in_category(k)
                 n_obs(sub(k)+j) = sum(categ_mat(:, k) == j);
-                sum_obs(sub(k)+j) = sum(y(categ_mat(:, k) == j));
             end
         end
         sub(end) = sub(end-1) + levels_in_category(end);
         N = length(y);
         %% Using the last expectation as starting point to reduce burnin    
         mu = zeros(iter, sum(levels_in_category)+n_continuous);
+        % TODO: need to change when having multiple categorial predictors
         mu(1, 1:sum(levels_in_category)) = ...
-            Exp_mat(ID(i), 1:sum(levels_in_category));
-        mu(1, (sum(levels_in_category)+1):(sum(levels_in_category)+n_continuous)) = ...
-            Exp_mat(ID(i), route_max+1:end-1);
+            Exp_mat(ID(i), 1: sum(levels_in_category));
+        mu(1, (sum(levels_in_category)+1): (sum(levels_in_category) + n_continuous)) = ...
+            Exp_mat(ID(i), route_max+1: end-1);
         phi(1) = Exp_mat(ID(i), end);
         
         %% Gibbs sampling
@@ -57,19 +56,22 @@ function Exp_mat = KN_BUpdate(T_index, ID_mat, prior_mat, Exp_mat, vip_route)
         f_post_nu = @(post_xi, prior_xi, phi, mid, prior_nu)...
             (prior_nu.*prior_xi + mid.*phi)./post_xi;
         for t = 2: iter
+            % TODO: if the # of categorial predictors increases, should
+            % iterate the following part, change variables including n_obs
             % Update nu, hyperparameter of mu_i
-            post_phi = f_post_xi(vip_route(ID(i)), kappa, nu_phi);
-            mid_nu = sum(mu(t,1:vip_route(ID(i))));
+            post_phi = f_post_xi(sum(n_obs~=0), kappa, nu_phi);
+            mid_nu = sum(mu(t-1, n_obs~=0));
             post_mu = f_post_nu(post_phi, kappa, nu_phi, mid_nu, zeta);
             nu(t) = normrnd(post_mu, 1./sqrt(post_phi));
-            % Update mu_i of each route
-            
-            alpha = y - sum(conti_mat.*repmat(mu(t-1, sum(levels_in_category)+1:end-1),N,1),2)...
-                -sum(mu(t-1, categ_mat),2); 
-            
+            % Update mu_i of each route           
+            alpha = y - sum(conti_mat.*...
+                repmat(mu(t-1, (sum(levels_in_category) + 1): (end - 1)), N, 1), 2);
             for k = 1: n_category
-                alpha = alpha + mu(t-1, categ_mat(:, k))';               
-                post_phi = f_post_xi(n_obs(sub(k)+1:sub(k+1)),nu_phi,phi(t-1)); 
+                alpha = alpha - mu(t-1, categ_mat(:, k) + sub(k))';
+            end            
+            for k = 1: n_category
+                alpha = alpha + mu(t-1, categ_mat(:, k) + sub(k))';               
+                post_phi = f_post_xi(n_obs(sub(k) + 1: sub(k+1)), nu_phi, phi(t-1)); 
                 mid_categ = zeros(1, levels_in_category(k));
                 for j = 1: levels_in_category(k)
                     mid_categ(j) = sum(alpha(categ_mat(:,k)==j));
@@ -77,25 +79,26 @@ function Exp_mat = KN_BUpdate(T_index, ID_mat, prior_mat, Exp_mat, vip_route)
                 post_mu = ...
                     f_post_nu(post_phi, nu_phi, phi(t-1), mid_categ, nu(t));
                 mu(t, sub(k)+1:sub(k+1)) = normrnd(post_mu, 1./sqrt(post_phi));
-                alpha = alpha - mu(t-1, categ_mat(:, k))';
+                alpha = alpha - mu(t, categ_mat(:, k) + sub(k))';
             end        
             % Update beta, coefficients of continuous predictors
             for k = 1: n_continuous
-                alpha = alpha + conti_mat(:, k).*mu(t-1, sum(levels_in_category)+k) ;
-                post_phi = f_post_xi(conti_mat(:,k)'*conti_mat(:,k), alpha_phi, phi(t));
+                alpha = alpha + conti_mat(:, k) * mu(t-1, sum(levels_in_category) + k) ;
+                post_phi = f_post_xi(conti_mat(:, k)' * conti_mat(:, k), alpha_phi, phi(t-1));
                 mid_conti = sum(alpha, 1);
                 post_mu = f_post_nu(post_phi, alpha_phi, phi(t-1), mid_conti, alpha_mu);
                 mu(t, sum(levels_in_category)+k) = normrnd(post_mu, 1./sqrt(post_phi));
-                alpha = alpha - conti_mat(:, k).*mu(t, sum(levels_in_category)+k);
+                alpha = alpha - conti_mat(:, k) * mu(t, sum(levels_in_category) + k);
             end
             mid_err = sum(alpha.^2);
             % Update phi
-            phi(t) = f_update_phi(phi_a+mid_err/2, phi_b+N/2);
+            phi(t) = f_update_phi(phi_a + mid_err/2, phi_b + N/2);
         end
     
         %% post Gibbs sampling selection
-        Exp_mat(ID(i), [1:sum(levels_in_category),route_max+1:end-1]) = mean(mu(burnin:iter, :), 1);
-        Exp_mat(ID(i), end) = mean(phi(burnin:iter, :), 1);
+        Exp_mat(ID(i), [1: sum(levels_in_category),route_max+1: end-1])...
+            = mean(mu(burnin: iter, :), 1);
+        Exp_mat(ID(i), end) = mean(phi(burnin: iter, :), 1);
     end
 end
 
--- KN_Prior.m
+++ KN_Prior.m
@@ -1,6 +1,6 @@
 function [prior] = KN_Prior(n_continuous, nvip)
     prior = zeros(nvip, 5+2*n_continuous); 
     prior(:,2) = 0.1; prior(:,3) = 1; 
-    prior(:,4) = 0.2; prior(:,5) = 0.1;
+    prior(:,4) = 1/0.2; prior(:,5) = 0.1;
     prior(:,7) = 0.1; 
 end
